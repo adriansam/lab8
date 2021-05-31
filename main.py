daca493b3b203c8b3bd8fe04e6a61a6c21b535c5
Adrian <adriansam.pl3@gmail.com>
2021-05-31 12:25:43 +0200

    updateee

--- main.py
+++ main.py
@@ -116,3 +116,60 @@ import matplotlib.pyplot as plt
 # plt.legend(loc='center left')
 # plt.show()
 # zad5
+# df = pd.read_csv('iris.data', sep=',', decimal='.', header=None)
+# print(df)
+# # przygotowanie wektora kolorow
+# colors = np.random.randint(0, 50, len(df.index))
+# #przygotowanie wektora z rozmiarami 'kropek'
+# scale = [np.abs(df[0].iloc[x] - df[1].iloc[x]) for x in range(len(df.index))]
+# # można te wielkości troszeczke 'podrasować'
+# # scale = [np.abs(df[0],iloc[x] - df[1]/iloc[x]) * 5 for in range(len(df.index))]
+#
+# plt.scatter(df[0], df[1], c=colors, s=scale)
+# plt.xlabel('sepal length')
+# plt.ylabel('sepal width')
+# plt.show()
+# zad6
+xlsx = pd.ExcelFile('imiona.xlsx')
+df = pd.read_excel(xlsx, header=0)
+print(df)
+
+plt.subplot(1, 3, 1)
+grouped = df.groupby('Plec').agg({'Liczba': ['sum']}).unstack()
+
+grouped.plot.bar(color=['r','g'])
+plt.xlabel('Płeć')
+
+# wykres 2
+plt.subplot(1, 3, 2)
+x = df['Rok'].unique()
+kobiety = df[(df.Plec == 'K')].groupby('Rok').agg({'Liczba':['sum']}).values
+mezczyzni = df[(df.Plec == 'M')].groupby('Rok').agg({'Liczba':['sum']}).values
+print(mezczyzni)
+plt.plot(x, kobiety, label="Kobiety")
+plt.plot(x, mezczyzni, label="Mężczyźni")
+plt.ylabel('Liczba narodzonych dzieci')
+
+# wykres 3
+plt.subplot(1, 3, 3)
+x = df['Rok'].unique()
+# bez funkcji falteen matplotlib wyrzuca wyjątek, który informuje nas, że nie można
+# przekazywać parametru jako tablicy wielowymiarowej a w takiej postaci w tym przypadku
+# zwracany jest wektor y, korzystamy wiec z flatten() poznan przy okazji omawiania biblioteki numpy
+y. df.groupby('Rok').agg({'Liczba':['sum']}).values.flatten()
+
+plt.bat(x, y)
+# # Wyświetlamy cały wykres
+plt.show()
+#zad 7
+df = pd.read_csv('zamowienia.csv', sep=';')
+policzone = df.groupby('Sprzedawca')['Utarg'].sum()
+# explode
+explode = [0.0 for n in range(len(policzone.index))]
+# określamy które kawałki i o ile wysunąć, tu losujemy jeden
+explode[np.random.randint(0, len(policzone.index))] = 0.2
+policzone.plot.pie(subplots=True, autopct='%.2f %%', fontsize=8, explode=explode, shadows=True)
+plt.title("Procentowy udział zamówień sprzedawców")
+plt.show()
+
+
